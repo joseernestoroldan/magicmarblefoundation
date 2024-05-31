36d279eed565fc170e3954fc7e3fb9b14a41caab
Jose <joseernestoroldan@gmail.com>
2024-05-31 16:59:35 -0400

    board

--- utils/members.ts
+++ utils/members.ts
@@ -2,21 +2,21 @@ export const board = [
   {
     name: "Jennifer George",
     post: "President & Founder",
-    email: "Jennifer@email.com",
+    email: "Jennifer@MagicMarbleFoundation.org",
     body: "Jennifer George leads initiatives at Magic Marble Foundation to promote human and non-human welfare through education, animal rescue, equal rights advocacy, and environmental activism. With a background deeply rooted in activism and animal rescue, Jennifer has spearheaded projects spanning 7 countries and 4 continents. These initiatives include vegan feeding programs for Covid relief, educational access for underserved communities, public campaigns for animal rights, and free veterinary care to alleviate the suffering of homeless animals. She aims to foster a more compassionate and inclusive world for both humans and non-humans alike.",
     image: "/President.webp"
   },
   {
     name: "Maryanne George",
     post: "Secretary",
-    email: "Jennifer@email.com",
+    email: "info@MagicMarbleFoundation.org",
     body: "Maryanne George, a retired mental health nurse with a lifelong dedication to animal welfare, traces her advocacy back to her childhood on a farm in Kerala, India. Her passion for championing animal rights has led her to engage in various initiatives, from advocating for elephant liberation to spearheading wildlife rescue efforts and Trap-Neuter-Return (TNR) programs. She has played a pivotal role in disaster relief efforts, offering her service to alleviate the suffering of both humans and animals alike. Maryanne directs the TNR program in Michigan and manages a sanctuary providing care for small wildlife and companion animals.",
     image: "/secretary.webp"
   },
   {
     name: "Manjool Shah",
     post: "Vice President",
-    email: "Jennifer@email.com",
+    email: "Manjool@gmail.com ",
     body: "Manjool Shah is a board certified ophthalmologist practicing in New York City. His career has been focused on educating the next generation of ophthalmologists as well as helping provide care and advancing glaucoma surgical innovation in the United States and in underserved countries around the world.",
     image: "/VicePresident.webp"
   },
