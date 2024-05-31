fcb82c5e4946401938ed96707674c95da2af4a21
Jose <joseernestoroldan@gmail.com>
2024-05-31 15:01:02 -0400

    white

--- components/parallax/parallax.tsx
+++ components/parallax/parallax.tsx
@@ -3,7 +3,7 @@ import { Parallax } from "react-parallax";
 import type { ParallaxProps } from "@/types/types";
 
 const ParallaxContainer = ({ children, bgImage, opacity }: ParallaxProps) => (
-  <div className="w-full overflow-hidden relative py-64 bg-cyan-200">
+  <div className="w-full overflow-hidden relative py-64 bg-white">
     <div className="absolute w-[2600px] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
       <Parallax
         className="w-full bg-cover bg-top flex items-center justify-center"
