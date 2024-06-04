0fcb91383612f5811dff5d9a29df64a1e26e4487
Jose <joseernestoroldan@gmail.com>
2024-06-03 20:29:31 -0400

    member

--- utils/members.ts
+++ utils/members.ts
@@ -4,21 +4,21 @@ export const board = [
     post: "President & Founder",
     email: "Jennifer@MagicMarbleFoundation.org",
     body: "Jennifer George leads initiatives at Magic Marble Foundation to promote human and non-human welfare through education, animal rescue, equal rights advocacy, and environmental activism. With a background deeply rooted in activism and animal rescue, Jennifer has spearheaded projects spanning 7 countries and 4 continents. These initiatives include vegan feeding programs for Covid relief, educational access for underserved communities, public campaigns for animal rights, and free veterinary care to alleviate the suffering of homeless animals. She aims to foster a more compassionate and inclusive world for both humans and non-humans alike.",
-    image: "/President.webp"
+    image: "/President.webp",
   },
   {
     name: "Maryanne George",
     post: "Secretary",
     email: "info@MagicMarbleFoundation.org",
     body: "Maryanne George, a retired mental health nurse with a lifelong dedication to animal welfare, traces her advocacy back to her childhood on a farm in Kerala, India. Her passion for championing animal rights has led her to engage in various initiatives, from advocating for elephant liberation to spearheading wildlife rescue efforts and Trap-Neuter-Return (TNR) programs. She has played a pivotal role in disaster relief efforts, offering her service to alleviate the suffering of both humans and animals alike. Maryanne directs the TNR program in Michigan and manages a sanctuary providing care for small wildlife and companion animals.",
-    image: "/secretary.webp"
+    image: "/secretary.webp",
   },
   {
     name: "Manjool Shah",
     post: "Vice President",
     email: "Manjool@gmail.com ",
     body: "Manjool Shah is a board certified ophthalmologist practicing in New York City. His career has been focused on educating the next generation of ophthalmologists as well as helping provide care and advancing glaucoma surgical innovation in the United States and in underserved countries around the world.",
-    image: "/VicePresident.webp"
+    image: "/VicePresident.webp",
   },
 ];
 
@@ -28,7 +28,7 @@ export const countryCoordinator = [
     post: "Uganda country coordinator",
     email: "MMFUganda@gmail.com",
     body: "Fahd Kharim Ssevume, widely recognized by his alias Dash Meizler, stands as a distinguished figure celebrated for his unwavering commitment to humanitarianism and fervent advocacy for animal rights. Renowned as the founder of the Podrska Foundation, Dash Meizler has played a pivotal role in propelling the vegan movement forward. Dash Meizler's remarkable affinity for both human and non-human animals has served as a driving force behind his contributions to the cause of veganism. His ambitions are worldwide, and he embodies principles that are perfectly aligned with Magic Marble Foundation’s purpose of high-level, productive, and long-term transformation.",
-    image: "/countryUganda.webp"
+    image: "/countryUganda.webp",
   },
 
   {
@@ -36,18 +36,23 @@ export const countryCoordinator = [
     post: "Nepal country coordinator",
     email: "Jennifer@email.com",
     body: "Kilamu Sherpa brings invaluable expertise gained from years of hands-on experience as an impassioned animal rescuer. With a lifelong commitment to rescuing street animals dating back to her childhood, Kilamu's deep-seated compassion for both humans and animals informs her leadership style. Her dedication to serving those in need has left an indelible mark on our efforts to create positive change in Nepal.",
-    image: "/countryNepal.webp"
+    image: "/countryNepal.webp",
   },
-
-
 ];
- 
+
 export const advisoryBoard = [
   {
     name: "Dash Meizler",
-    post: "Uganda country coordinator",
+    post: "Advisory Board",
     email: "Jennifer@email.com",
     body: "Fahd Kharim Ssevume, widely recognized by his alias Dash Meizler, stands as a distinguished figure celebrated for his unwavering commitment to humanitarianism and fervent advocacy for animal rights. Renowned as the founder of the Podrska Foundation, Dash Meizler has played a pivotal role in propelling the vegan movement forward, leaving an indelible mark on the landscape of compassionate living. Dash Meizler's remarkable affinity for both human and non-human animals has served as a driving force behind his contributions to the cause of veganism. His ambitions are worldwide, and he embodies principles that are perfectly aligned with magic marble Marble Foundation's purpose of high-level, productive, and long-term transformation.",
-    image: "/countryUganda.webp"
+    image: "/countryUganda.webp",
+  },
+  {
+    name: "Cathy Caruth",
+    post: "Advisory Board",
+    email: "Jennifer@email.com",
+    body: "Cathy Caruth teaches in the Departments of English and Comparative Literature at Cornell University. She focuses on the languages of trauma and testimony, on literary theory, and on contemporary discourses concerning the annihilation and survival of language. Her most recent books are Literature in the Ashes of History (Hopkins, 2013) and Listening to Trauma: Conversations with Leaders in the Theory and Treatment of Catastrophic Experience.",
+    image: "/caruth.webp",
   },
 ];
