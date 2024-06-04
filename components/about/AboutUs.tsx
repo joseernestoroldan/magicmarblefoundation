d471fc23c47a345b13fb93a7f7972eb85f103ee7
Jose <joseernestoroldan@gmail.com>
2024-06-04 16:23:43 -0400

    about us

--- components/about/AboutUs.tsx
+++ components/about/AboutUs.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import Heading from "../headings/heading";
 import Photo from "../photo/photo";
 import HeadingAboutAnimation from "../headingsAnimations/HeadingAboutAnimation";
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
     <HeadingAboutAnimation>
-      <div className="flex flex-col-reverse justify-end md:flex-row md:items-center md:justify-center w-full h-screen">
+      <div className="flex flex-col-reverse justify-end md:flex-row md:items-center md:justify-center w-full h-screen bg-blue-300">
         <div className="w-full md:w-[50%] flex flex-col justify-between md:justify-center items-center space-y-10">
           <div className="w-full relative text-wrap pt-8 md:pt-0">
             <Heading
@@ -29,14 +30,18 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="w-full md:w-[50%] flex flex-col justify-center items-center">
-          <Photo
-            objectFit="object-cover"
-            width=" w-[220px] md:w-[330px]"
-            height="h-[220px] md:h-[330px]"
-            src="/logo.jpg"
-            alt="Magic Marbel Foundation"
-            borderRadius=""
-          />
+
+          <div className="w-[220px] md:w-[330px] h-[220px] md:h-[330px] relative">
+            {/* photo container */}
+            <Image
+              className="object-cover object-top"
+              src="/logo.jpg"
+              fill
+              alt="Magic Marbel Foundation"
+              priority={true}
+            />
+          </div>
+
         </div>
       </div>
     </HeadingAboutAnimation>
