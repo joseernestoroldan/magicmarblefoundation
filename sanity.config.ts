6cc8682593afc8f61e6fba874478d4e279fadb77
Jose <joseernestoroldan@gmail.com>
2024-06-10 19:14:14 -0400

    admin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "sanity";
+import { structureTool } from 'sanity/structure'
 
 const config = defineConfig({
   projectId: "7k587yzs",
@@ -6,6 +7,8 @@ const config = defineConfig({
   title: "magicmarblefoundation",
   apiVersion: "2023-03-04",
   basePath: "/admin",
+  plugins: [structureTool()]
 });
 
 export default config;
+ 
