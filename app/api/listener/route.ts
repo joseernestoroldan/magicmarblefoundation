c7098500361286f87d30a24aba53642870094442
Jose <joseernestoroldan@gmail.com>
2024-08-09 20:07:41 -0400

    test4

--- app/api/listener/route.ts
+++ app/api/listener/route.ts
@@ -1,17 +1,5 @@
-import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook';
 
-const handler = async (req: any, res: any) => {
-
-//authenticating the webhook
-  try {
-    //getting payload
-    const { _id } = req.body;
-    console.log(_id)
-    
-    res.status(200).json({ msg: 'Product pages revalidated.' });
-  } catch (error) {
-    res.status(500).json({ err: 'Something went Wrong!' });
-  }
-};
-
-export default handler;
+export async function POST(request: Request) {
+  const res = await request.json()
+  return Response.json({ res })
+}
