1d89febec45b842ed484f30100a7f061985939ec
Jose <joseernestoroldan@gmail.com>
2024-08-18 02:28:37 -0400

    carousel

--- app/gallery/GalleryCarousel.tsx
+++ app/gallery/GalleryCarousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef, useCallback } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import EnterSection from "@/components/animations/enterSection/EnterSection";
 import Image from "next/image";
 
@@ -8,44 +8,76 @@ import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 const GalleryCarousel = ({ gallery }: any) => {
   const [count, setCount] = useState<number>(0);
-  const [isTouched, setIsTouched] = useState<boolean>(false);
+  const [dragDirection, setDragDirection] = useState<string>('');
+  const [dragStarted, setDragStarted] = useState(false); // Flag to track drag initiation
   const elementRef = useRef<HTMLDivElement>(null);
 
   const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
-    setIsTouched(true);
+    setDragStarted(true); // Set drag flag on touch start
     const touch = event.touches[0];
     const initialX = touch.clientX;
     elementRef.current!.dataset.initialX = String(initialX);
   };
 
   const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (!dragStarted) return; // Ignore move events if not dragging
     const touch = event.touches[0];
     const currentX = touch.clientX;
     const initialX = parseFloat(elementRef.current!.dataset.initialX || "0");
     const threshold = 50; // Adjust threshold as needed
 
-    if (isTouched) {
-      if (currentX > initialX + threshold) {
-        handlePrev();
-      } else {
-        handleNext();
-      }
+    if (currentX > initialX + threshold) {
+      setDragDirection('left');
+    } else if (currentX < initialX - threshold) {
+      setDragDirection('right');
+    } else {
+      setDragDirection(''); // Reset drag direction if not exceeding threshold
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setDragStarted(false); // Reset drag flag on touch end
+
+    if (dragDirection === 'left') {
+      handlePrev();
+      setDragDirection(''); // Reset drag direction
+    } else if (dragDirection === 'right') {
+      handleNext();
+      setDragDirection(''); // Reset drag direction
     }
   };
 
   const handlePrev = () => {
     if (count > 0) {
-      setCount(count - 1);
-      setIsTouched(false);
+      setCount((prevCount) => prevCount - 1);
     }
+    (prevIndex) => Math.max(prevIndex - 1, 0)
   };
 
   const handleNext = () => {
     if (count < gallery.length) {
-      setCount(count + 1);
-      setIsTouched(false);
+      setCount((prevCount) => prevCount + 1);
     }
   };
+
+  useEffect(() => {
+    if (dragDirection === 'left') {
+      // Handle previous slide logic
+      console.log('Previous slide');
+     // handleNext()
+      setCount((prevIndex) => Math.max(prevIndex - 1, 0));
+    } else if (dragDirection === 'right') {
+      // Handle next slide logic
+      console.log('Next slide');
+      //handlePrev()
+      setCount((prevIndex) => Math.min(prevIndex + 1, gallery.length));
+    }
+    setDragDirection(''); // Reset drag direction
+    }, []);
+    
+ 
+
+
   return (
     <EnterSection>
       <div className="relative w-full h-auto flex flex-col justify-start items-center p-0">
@@ -61,6 +93,7 @@ const GalleryCarousel = ({ gallery }: any) => {
                 ref={elementRef}
                 onTouchStart={handleTouchStart}
                 onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}
               >
                 <Image
                   className={`object-cover
