dde6b5df6ee489e7090d2a3a0c497301a2914f31
Jose <joseernestoroldan@gmail.com>
2024-09-16 10:07:55 -0400

    toppicks

--- components/topPicks/TopPicks.tsx
+++ components/topPicks/TopPicks.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import { getAllByTop } from "@/client";
 import TopPicksCarousell from "./TopPicksCarousell";
