28f3362fa7e69a6e1749bc1611b4db4500cb00c8
Jose Ernesto <joseernestoroldan@gmail.com>
2025-01-31 13:01:45 -0400

    Styles Update: ReelPosts

--- components/portalPaddyField/ReelPosts.tsx
+++ components/portalPaddyField/ReelPosts.tsx
@@ -1,82 +1,53 @@
+import { QueryType } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ReelPosts = ({ paddyFieldPosts }: any) => {
+const ReelPosts = ({ paddyFieldPosts }: { paddyFieldPosts: QueryType[] }) => {
+  const [
+    mainArticle,
+    firstArticle,
+    secondArticle,
+    thridArticle,
+    fourthArticle,
+    ...rest
+  ] = paddyFieldPosts;
+
   return (
-    <div className="w-full h-auto flex flex-wrap justify-center gap-8 lg:justify-between lg:gap-0">
-      <Link href={`paddyfield/${paddyFieldPosts[1]._id}`}>
-        <div className="w-[300px]  flex flex-col items-center space-y-4 hover:brightness-75 cursor-pointer">
-          {paddyFieldPosts[1].mainImage && (
-            <div className="w-[300px] h-[300px] bg-gray-600 mt-8 relative rounded-[5px] overflow-hidden">
-              <Image
-                src={paddyFieldPosts[1].mainImage}
-                alt="portal paddy field"
-                fill
-                className="object-cover"
-              />
-            </div>
-          )}
-          <div className=" w-300px flex flex-col space-y-4">
-            <h2 className="z-10 w-[300px] text-green-950 text-xl font-medium">
-              {paddyFieldPosts[1].title}
-            </h2>
-            <p className="w-[300px] text-green-950 text-base text-opacity-85">
-              {paddyFieldPosts[1].description}
-            </p>
-          </div>
-        </div>
-      </Link>
-
-      <Link href={`paddyfield/${paddyFieldPosts[2]._id}`}>
-        <div className="w-300px flex flex-col items-center space-y-4 hover:brightness-75 cursor-pointer">
-          {paddyFieldPosts[2].mainImage && (
-            <div className="w-[300px] h-[300px] bg-gray-600 mt-8 relative rounded-[5px] overflow-hidden">
-              <Image
-                src={paddyFieldPosts[2].mainImage}
-                alt="portal paddy field"
-                fill
-                className="object-cover"
-              />
-            </div>
-          )}
-
-          <div className=" w-300px flex flex-col space-y-4">
-            <h2 className="z-10 w-[300px] text-green-950 text-xl font-medium">
-              {paddyFieldPosts[2].title}
-            </h2>
-            <p className="w-[300px] text-green-950 text-base text-opacity-85">
-              {paddyFieldPosts[2].description}
-            </p>
-          </div>
-        </div>
-      </Link>
-
-      <Link href={`paddyfield/${paddyFieldPosts[3]._id}`}>
-        <div className="w-300px flex flex-col items-center space-y-4 hover:brightness-75 cursor-pointer">
-          {paddyFieldPosts[3].mainImage && (
-            <div className="w-[300px] h-[300px] bg-gray-600 mt-8 relative rounded-[5px] overflow-hidden">
-              <Image
-                src={paddyFieldPosts[3].mainImage}
-                alt="portal paddy field"
-                fill
-                className="object-cover"
-              />
-            </div>
-          )}
-
-          <div className=" w-300px flex flex-col space-y-4">
-            <h2 className="z-10 w-[300px] text-green-950 text-xl font-medium">
-              {paddyFieldPosts[3].title}
-            </h2>
-            <p className="w-[300px] text-green-950 text-base text-opacity-85">
-              {paddyFieldPosts[3].description}
-            </p>
-          </div>
-        </div>
-      </Link>
+    <div className="w-full h-auto flex flex-wrap justify-center gap-8">
+      <Post article={firstArticle} />
+      <Post article={secondArticle} />
+      <Post article={thridArticle} />
+      <Post article={fourthArticle} />
     </div>
   );
 };
 
 export default ReelPosts;
+
+const Post = ({ article }: { article: QueryType }) => {
+  return (
+    <Link href={`paddyfield/${article._id}`}>
+      <div className="w-[300px] sm:w-[400px] h-[570px] rounded-[10px] flex flex-col items-center hover:brightness-75 bg-black bg-opacity-10 hover:bg-opacity-20 cursor-pointer mt-8">
+        {article.mainImage && (
+          <div className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] relative rounded-[5px] overflow-hidden">
+            <Image
+              src={article.mainImage}
+              alt="portal paddy field"
+              fill
+              className="object-cover"
+            />
+          </div>
+        )}
+        <div className=" w-[300px] sm:w-[400px] flex flex-col space-y-2">
+          <h2 className="z-10 w-[300px] sm:w-[400px] text-green-950 text-xl text-center font-semibold">
+            {article.title}
+          </h2>
+          <p className="w-[300px] sm:w-[400px] text-green-950 text-base text-opacity-85 px-4">
+            {article.description}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
