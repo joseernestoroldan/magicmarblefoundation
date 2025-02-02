5a894580d27018eedb1d0f159f657d8537337c1c
Jose Ernesto <joseernestoroldan@gmail.com>
2025-02-02 15:09:16 -0400

    Paragraphs Styles | Dynamic Page Portal Paddy Field | Navbar Link

--- app/paddyfield/morerecipies/[id]/page.tsx
+++ app/paddyfield/morerecipies/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getOne } from "@/client";
 import Paragraphs from "@/components/parrafos/Paragraphs";
-import { QueryType } from "@/types/types";
+import { HotSpot, QueryType } from "@/types/types";
 import Image from "next/image";
 
 const RecipiePage = async ({
@@ -8,18 +8,67 @@ const RecipiePage = async ({
 }: {
   params: { id: string };
 }) => {
-  
   const query: QueryType[] | null = await getOne(_Id);
   if (!query) return null;
   const [data] = query;
   const {} = data;
-  const {ingredients, instructions} = data;
+  const {
+    ingredients,
+    instructions,
+    mainImage,
+    secondImage,
+    thirdImage,
+    fourthImage,
+    hotSpotMain,
+    hotSpotSecond,
+    hotSpotThird,
+    hotSpotFourth,
+  } = data;
 
   return (
-    <div className="w-full h-screen bg-gradient-to-b from-white via-green-300 to-white flex flex-col items-center justify-center space-y-8">
-      {ingredients && <Paragraphs contenido={ingredients}/>}
+    <div className="w-full bg-gradient-to-b from-white via-green-300 to-white flex flex-row">
+      <div className="p-12 w-2/5 flex flex-col items-center space-y-4 min-w-[300px]">
+        {mainImage && <ItemGallery image={mainImage} hotSpot={hotSpotMain}/>}
+        {secondImage && <ItemGallery image={secondImage} hotSpot={hotSpotSecond} />}
+        {thirdImage && <ItemGallery image={thirdImage} hotSpot={hotSpotThird} />}
+        {fourthImage &&  <ItemGallery image={fourthImage} hotSpot={hotSpotFourth} />}
+      </div>
+      <div className=" w-3/5 flex flex-col justify-start space-y-4 px-12">
+        <h2 className="text-gray-500 font-bold text-2xl">Ingredients:</h2>
+        {ingredients && (
+          <Paragraphs
+            contenido={ingredients}
+            sizeText="text-[16px]"
+            mdSizeText="text-[18px]"       
+          />
+        )}
+        <h2 className="text-gray-500 font-bold text-2xl">Instructions:</h2>
+        {instructions && (
+          <Paragraphs
+            contenido={instructions}
+            sizeText="text-[16px]"
+            mdSizeText="text-[18px]"
+          />
+        )}
+      </div>
     </div>
   );
 };
 
 export default RecipiePage;
+
+const ItemGallery = ({ image, hotSpot }: { image: string | null, hotSpot:HotSpot | null }) => {
+  const x = (hotSpot?.x ?? 0.5) * 100;
+  const y = (hotSpot?.y ?? 0.5) * 100;
+  return (
+    <div className="w-full bg-gray-600 aspect-square relative rounded-[10px] overflow-hidden">
+      <Image
+        src={image ?? "/no_profile.webp"}
+        alt="Recipies Magic Marble Foundation"
+        fill
+        className="object-cover"
+        style={{ objectPosition: `${x}% ${y}%` }}
+      />
+    </div>
+  );
+};
--- components/parrafos/Paragraphs.tsx
+++ components/parrafos/Paragraphs.tsx
@@ -1,23 +1,26 @@
 import { Block } from "@/types/types";
 
-const Paragraphs = ({ contenido }: { contenido: Block[] }) => {
+const Paragraphs = ({ contenido, sizeText, mdSizeText, indent }: { contenido: Block[], sizeText?: string, mdSizeText?: string, indent?:string }) => {
   return (
     <div className="">
       {contenido.map((item: Block, index: number) => {
         const unTrimmedText = item.children[0].text;
         const text = unTrimmedText.trim();
         const style = item.style;
-        const textSize = decideSize(style);
+        const textSize = decideSize(style, sizeText, mdSizeText, indent);
+        console.log(textSize)
 
         if (text === "##") {
-          return <div key={index} className="h-[20px] w-full"></div>;
+          return <div key={index} className="h-[16px] w-full"></div>;
         }
-
-        if (text !== "") {
+        if (text === "#") {
+          return <div key={index} className="h-[5px] w-full"></div>;
+        }
+        if ((text !== "##") && (text !== "#")) {
           return (
             <div key={index}>
               <div
-                className={`space-y-2 indent-8 text-gray-500 font-semibold text-justify ${textSize}`}
+                className={`space-y-2 text-gray-500 font-semibold text-justify text-wrap w-full ${textSize}`}
               >
                 {text}
               </div>
@@ -31,7 +34,11 @@ const Paragraphs = ({ contenido }: { contenido: Block[] }) => {
 
 export default Paragraphs;
 
-const decideSize = (style: string) => {
+const decideSize = (style: string, sizeText?: string, mdSizeText?: string, indentText?: string) => {
+
+  const size = sizeText || "text-[16px]"
+  const mdSize = mdSizeText || "text-[18px]"
+  const indent = indentText || "indent-0"
   switch (style) {
     case "h6":
       return "text-[22px] leading-[44px]";
@@ -46,7 +53,7 @@ const decideSize = (style: string) => {
     case "h1":
       return "text-[42px] leading-[84px]";
     case "normal":
-      return "text-[20px] leading-[22px]";
+      return `${size} leading-[20px] ${mdSize} ${indent}`;
     default:
       return "text-[20px] leading-[20px]";
   }
