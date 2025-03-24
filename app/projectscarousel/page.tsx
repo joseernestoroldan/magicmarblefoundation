4403cadf77ec6fd86259984df4226f79aeffba8a
jose <joseernestoroldan@gmail.com>
2025-03-24 11:52:25 -0400

    retoch projects

--- app/projectscarousel/page.tsx
+++ app/projectscarousel/page.tsx
@@ -27,12 +27,12 @@ const ProjectsCarouselPage = async () => {
             title="Our Projects"
             color="text-cyan-500"
             shadow=""
-            textSize="text-5xl"
+            textSize="text-4xl"
             center="flex justify-center "
           />
           <EnterElement>
             <div className="relative w-full h-[600px] flex justify-center overflow-hidden pb-24">
-              <Carousel className="w-full max-w-[350px] min-[425px]:max-w-[423px] md:max-w-[766px] lg:max-w-[900px] text-cyan-600 font-bold">
+              <Carousel className="w-full max-w-[350px] min-[425px]:max-w-[423px] md:max-w-[766px] lg:max-w-[1200px] text-cyan-600 font-bold">
                 <CarouselContent className="rounded-2xl">
                   {projects.map((project: any, index: number) => (
                     <CarouselItem key={index} className="border-0 rounded-2xl">
@@ -42,22 +42,22 @@ const ProjectsCarouselPage = async () => {
                             <div className="text-4xl font-semibold w-full h-full relative">
                               <div className="absolute z-10 left-1/2 -translate-x-1/2 md:-translate-x-0 md:left-5 bottom-10  flex flex-col md:flex-row justify-start md:justify-between items-center w-full">
                                 <div className=" bg-black bg-opacity-70 w-[300px] min-[425px]:w-[400px] p-4 rounded-2xl space-y-4">
-                                  <h1 className="text-white text-xl min-[425px]:text-2xl font-bold w-full text-center">
+                                  <h1 className="text-white text-lg min-[425px]:text-xl font-bold w-full text-center">
                                     {project.title}
                                   </h1>
-                                  <p className="text-white text-base min-[425px]:text-lg font-medium text-wrap w-full indent-4">
+                                  <p className="text-white text-base min-[425px]:text-base font-medium text-wrap w-full indent-4">
                                     {project.description}
                                   </p>
                                   <div className="flex justify-around items-center">
                                     <Link
                                       href={`projects/project/${project._id}`}
                                     >
-                                      <p className="text-cyan-300 text-base  font-bold underline">
+                                      <p className="text-cyan-300 text-sm font-bold underline">
                                         About This Project
                                       </p>
                                     </Link>
                                     <Link href={`projects`}>
-                                      <p className="text-cyan-300 text-base  font-bold underline">
+                                      <p className="text-cyan-300 text-sm  font-bold underline">
                                         More Projects
                                       </p>
                                     </Link>
