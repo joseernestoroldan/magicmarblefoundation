e5312dd7e19ea2b60c1799d3c9c9b15fb75b38a0
jose <joseernestoroldan@gmail.com>
2025-10-02 14:13:45 -0400

    image issue email templates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import Footer from "@/components/footer/Footer";
 import localFont from "next/font/local";
 import { getNameSession, getSessionId } from "@/lib/sessions";
 import Badge from "@/components/badge/Badge";
-
-// import openSans font from google fonts
 import { Open_Sans } from "next/font/google";
 
 
@@ -13,6 +11,7 @@ const openSansFont = Open_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
+
 const myFont = localFont({ src: "../public/bellmedium.woff2" });
 
 export default async function RootLayout({
